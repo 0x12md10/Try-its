6f28467fcb7eabf3107907cc089e05e513538a20
0x12md10 <abish102001@gmail.com>
2024-07-12 22:40:03 +0530

    added store

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
 
-import React, { Suspense, useContext, useState } from 'react';
+// import React, { Suspense, useContext, useState } from 'react';
 
 // const Dashboard = React.lazy(()=> import("./Dashboard"));
 // const Landing = React.lazy(()=> import("./Landing"))
 
-import Dashboard from "./Dashboard";
-import Landing from "./Landing";
+// import Dashboard from "./Dashboard";
+// import Landing from "./Landing";
 import './App.css'
-import { CountContext } from './context';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Nav from './Nav';
+// import { CountContext } from './context';
+// import { BrowserRouter,Route,Routes } from 'react-router-dom';
+// import Nav from './Nav';
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil';
+import { counterAtom } from './store/atoms/count';
 
 function App() {
 
 
 
   return (
-    <BrowserRouter>
-      <CountContext.Provider value={0} >
-        <Routes>
-          <Route path='/' element={<Suspense fallback={"loading...."}><Nav/></Suspense>}> 
-            <Route index element={<Suspense fallback={"loading...."}><Landing/></Suspense>}/>
-            <Route path='/dashboard' element={<Suspense fallback={"loading...."}><Dashboard/></Suspense>}/>
-          </Route>
-          
-        </Routes>
-      </CountContext.Provider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <div>
+        <Count />
+      </div>
+    </RecoilRoot>
   )
 }
 
+function Count() {
+  const count = useRecoilValue(counterAtom);
+  return (
+    <div>
+      <div>Count:{count} </div>
+      <Button />
+    </div>
+  )
+}
+
+function Button() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  const handleIncrement = ()=> {
+    setCount(count => count+1)
+  }
+
+  const handleDecrement = ()=> {
+    setCount(count => count -1)
+  }
+
+  return (
+    <div> 
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  )
+}
 
 
 
